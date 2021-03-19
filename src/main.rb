2d9a9d165064dd85ad1fe5cc2e6cd65e4d6c2969
theprab <prabut1@tcnj.edu>
2021-03-18 23:56:49 -0400

    comments for later changes

--- src/main.rb
+++ src/main.rb
@@ -5,12 +5,17 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+#create a resources section with a paragraph citing information and resources for homeless people during COVID
+#reasoning for and motivation behind project, include links to charities, orgs
+
 def add_item_to_site()
     #add requested item to site for someone to donate
+    #create a button here for Thursday on the site
 end
 
 def remove_item_from_site()
     #remove item once donated items have been inventoried
+    #create a button here for Thursday
 end
 
 def view_items()
