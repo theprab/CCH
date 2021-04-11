84da31e5d6bf750f772fad8a3d768964b67b5d38
theprab <prabut1@tcnj.edu>
2021-04-11 13:08:53 -0400

    created users with roles and posts

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -7,10 +7,12 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  resources :posts
+  root to: 'pages#home'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # The root page, e.g. www.example.com/, is sent here
   # root 'controller#method_in_controller'
-  root 'home#index'
+  #root 'home#index'
   # Devise authentification pages. This controlls the user login
   # and authentification system.
   devise_for :users
--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_190602) do
+ActiveRecord::Schema.define(version: 2021_04_09_022550) do
 
   create_table "app_configs", force: :cascade do |t|
     t.string "name"
     t.string "logo_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,8 +36,10 @@ ActiveRecord::Schema.define(version: 2020_01_13_190602) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "admin_boolean", default: "f"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
