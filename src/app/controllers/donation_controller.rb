79f7da1d2e028f5a15589d2646cc551c5788d26a
theprab <prabut1@tcnj.edu>
2021-04-26 21:11:36 -0400

    updates to html content of resources page and added authentication lock to admin access page

--- src/app/controllers/donation_controller.rb
+++ src/app/controllers/donation_controller.rb
@@ -1,3 +1,13 @@
 class DonationController < ApplicationController
     skip_before_action :authenticate_user!
+    before_action :load_posts, only: :index
+
+    def show
+        render template: "pages/#{params[:page]}"
+      end
+    
+    def load_posts
+        @posts = Post.order("created_at desc").limit(5)
+      end
+
 end
